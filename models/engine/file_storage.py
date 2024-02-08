40983d78f4c9430902f53d087d177ee1e59ad786
KelvinChukwuka <Koolkt10@gmail.com>
2024-02-09 00:01:14 +0100

    reload worked on

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,8 +36,15 @@ class FileStorage:
     def reload(self):
             """Deserializes a JSON file to an object"""
             from models.base_model import BaseModel
+            from models.amenity import Amenity
+            from models.city import City
+            from models.place import Place
+            from models.review import Review
+            from models.state import State
+            dictionary = {'BaseModel': BaseModel, 'City': City, 'Amenity': Amenity,
+                          'Place': Place, 'Review': Review, 'State': State}
 
             if os.path.exists(FileStorage.__file_path):
                 with open(FileStorage.__file_path, 'r') as f:
                     for key, value in json.load(f).items():
-                        self.new(BaseModel[value[__class__]](**value))
+                        self.new(dictionary[value[__class__]](**value))
