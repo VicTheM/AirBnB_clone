ef65b85dc2b814d0f5391f55cd108d7a6ec35aa3
Kelvin Chukwuka <temp@Kelvins-MacBook-Pro.local>
2024-02-08 23:34:33 +0100

    more classes

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    City Module
+"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City Class that inherits from BaseModel"""
+    name = ""
+    state_id = ""
---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Review Module
+"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review Class that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
